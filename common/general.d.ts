a22bc5a8777be8e90486f01ba2a9cff3ef67cccb
Shush <almogosheriko@gmail.com>
2021-05-12 17:42:52 +0300

    Update references

--- common/general.d.ts
+++ common/general.d.ts
@@ -1,25 +1,27 @@
+/// <reference types="typescript-to-lua/language-extensions" />
+
 declare const enum TalentStatus {
-    LEARNED = 0,
-    NOT_LEARNED = 1,
-    UNLEARNABLE = 2,
-    CAN_BE_LEARNED = 3,
+	LEARNED = 0,
+	NOT_LEARNED = 1,
+	UNLEARNABLE = 2,
+	CAN_BE_LEARNED = 3,
 }
 
 interface CustomNetTableDeclarations {
-    // Just an example of a nettable, not actually used
-    spider_manager: { spiders: number };
+	// Just an example of a nettable, not actually used
+	spider_manager: { spiders: number };
 }
 
 interface CustomGameEventDeclarations {
-    learn_talent_event: { ability: EntityIndex };
-    confirm_talent_learned: { talent_num: number; learned_by_force: 0 | 1 };
-    request_currently_selected_unit: {};
-    send_currently_selected_unit: { unit: EntityIndex };
-    ping_talent: { ability: EntityIndex; status: TalentStatus };
-    custom_chat_message: {
-        isTeam: boolean;
-        textData: string;
-        playerID: PlayerID;
-        ability_name: string | undefined;
-    };
+	learn_talent_event: { ability: EntityIndex };
+	confirm_talent_learned: { talent_num: number; learned_by_force: 0 | 1 };
+	request_currently_selected_unit: {};
+	send_currently_selected_unit: { unit: EntityIndex };
+	ping_talent: { ability: EntityIndex; status: TalentStatus };
+	custom_chat_message: {
+		isTeam: boolean;
+		textData: string;
+		playerID: PlayerID;
+		ability_name: string | undefined;
+	};
 }
--- game/scripts/vscripts/lib/manual_interface_declarations.ts
+++ game/scripts/vscripts/lib/manual_interface_declarations.ts
@@ -1,97 +1,83 @@
 declare interface ModifierTooltip {
-    fail_type: number;
+	fail_type: AttackRecord;
 }
 
 declare interface ModifierUnitEvent {
-    ability: CDOTABaseAbility;
-    target?: CDOTA_BaseNPC;
+	ability: CDOTABaseAbility;
+	target?: CDOTA_BaseNPC;
 }
 
 declare interface KnockbackProperties {
-    center_x: number;
-    center_y: number;
-    center_z: number;
-    duration: number;
-    knockback_duration: number;
-    knockback_distance: number;
-    knockback_height: number;
-    should_stun: 0 | 1;
+	center_x: number;
+	center_y: number;
+	center_z: number;
+	duration: number;
+	knockback_duration: number;
+	knockback_distance: number;
+	knockback_height: number;
+	should_stun: 0 | 1;
 }
 
 interface CDOTA_BaseNPC {
-    oldAddNewModifier: <TThis>(this: TThis, caster: CDOTA_BaseNPC | undefined, ability: CDOTABaseAbility | undefined, modifierName: string, modifierTable: object | undefined) => CDOTA_Buff;
-    active_item_modifiers: Map<string, CDOTA_Item[]>;
+	oldAddNewModifier: <TThis>(this: TThis, caster: CDOTA_BaseNPC | undefined, ability: CDOTABaseAbility | undefined, modifierName: string, modifierTable: object | undefined) => CDOTA_Buff;
+	active_item_modifiers: Map<string, CDOTA_Item[]>;
 }
 
 interface NewModifierProperties {
-    duration?: number;
-    ignoreStatusResistance?: 1;
-    [key: string]: any;
-}
-
-interface CustomGameEventDeclarations {
-    learn_talent_event: { ability: EntityIndex };
-    confirm_talent_learned: { talent_num: number; learned_by_force: 0 | 1 };
-    request_currently_selected_unit: {};
-    send_currently_selected_unit: { unit: EntityIndex };
-    ping_talent: { ability: EntityIndex; status: TalentStatus };
-    custom_chat_message: { isTeam: boolean; textData: string; playerID: PlayerID; ability_name: string | undefined };
-}
-
-interface CustomNetTableDeclarations {
-    // Just an example of a nettable, not actually used
-    spider_manager: { spiders: number };
+	duration?: number;
+	ignoreStatusResistance?: 1;
+	[key: string]: any;
 }
 
 interface CDOTA_BaseNPC_Hero {
-    talents_learned: Set<CDOTABaseAbility>;
-    talentMap: Map<number, CDOTABaseAbility>;
-    recently_buyback: boolean;
-    courier: CDOTA_Unit_Courier;
+	talents_learned: Set<CDOTABaseAbility>;
+	talentMap: Map<number, CDOTABaseAbility>;
+	recently_buyback: boolean;
+	courier: CDOTA_Unit_Courier;
 }
 
 interface CDOTA_Buff {
-    GetModifierLifeStealStacking(): number;
-    GetModifierStatusAmp(): number;
+	GetModifierLifeStealStacking(): number;
+	GetModifierStatusAmp(): number;
 }
 
 interface CDOTABaseAbility {
-    RequiresScepterForCharges(): boolean;
-    ExecuteOrderFilter(event: ExecuteOrderFilterEvent): boolean;
-    SetFrozenCooldown(state: boolean): void;
-    SetCooldownSpeed(speed: number): void;
+	RequiresScepterForCharges(): boolean;
+	ExecuteOrderFilter(event: ExecuteOrderFilterEvent): boolean;
+	SetFrozenCooldown(state: boolean): void;
+	SetCooldownSpeed(speed: number): void;
 }
 
 declare interface CreateIllusionsModifierKeys {
-    outgoing_damage?: number;
-    incoming_damage?: number;
-    bounty_base?: number;
-    bounty_growth?: number;
-    outgoing_damage_structure?: number;
-    outgoing_damage_roshan?: number;
-    duration?: number;
+	outgoing_damage?: number;
+	incoming_damage?: number;
+	bounty_base?: number;
+	bounty_growth?: number;
+	outgoing_damage_structure?: number;
+	outgoing_damage_roshan?: number;
+	duration?: number;
 }
 
 declare interface OrbData {
-    can_proc_from_illusions: boolean;
-    can_proc_while_silenced: boolean;
-    can_proc_on_building: boolean;
-    can_proc_on_wards: boolean;
-    can_proc_on_magic_immune: boolean;
-    mana_cost: number;
+	can_proc_from_illusions: boolean;
+	can_proc_while_silenced: boolean;
+	can_proc_on_building: boolean;
+	can_proc_on_wards: boolean;
+	can_proc_on_magic_immune: boolean;
+	mana_cost: number;
 }
 
 declare interface ModifierInstanceEvent {
-    attacker: CDOTA_BaseNPC;
-    damage: number;
-    damage_type: DamageTypes;
-    damage_category: DamageCategory;
-    damage_flags: DamageFlag;
-    inflictor?: CDOTABaseAbility;
-    original_damage: number;
-    ranged_attack: boolean;
-    unit: CDOTA_BaseNPC;
-    no_attack_cooldown: boolean;
-    record: number;
-    fail_type: AttackRecord;
+	attacker: CDOTA_BaseNPC;
+	damage: number;
+	damage_type: DamageTypes;
+	damage_category: DamageCategory;
+	damage_flags: DamageFlag;
+	inflictor?: CDOTABaseAbility;
+	original_damage: number;
+	ranged_attack: boolean;
+	unit: CDOTA_BaseNPC;
+	no_attack_cooldown: boolean;
+	record: number;
+	fail_type: AttackRecord;
 }
